73a17298c69f33721e6079945c9043bc301b697a
hackrush01 <atulshyan96@gmail.com>
2016-11-15 23:51:16 +0530

    popupjs: Expand search options for music and videos. Fixes #6.

--- popup.js
+++ popup.js
@@ -5,11 +5,11 @@ function register(){
 	var query = document.getElementById("query").value;
 
 	if(document.getElementById("music").checked == true)
-		set1='mp3';
+		set1='mp3|wav|m4a|ogg|wma|flac';
 	else
 		set1='';
 	if(document.getElementById("video").checked == true)
-		var set2='|mkv|mp4|avi';
+		var set2='|mkv|mp4|avi|webm|flv|mov|mpg|mpeg';
 	else
 		set2='';
    	if(document.getElementById("books").checked == true)
