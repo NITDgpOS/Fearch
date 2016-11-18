df28e71a893b471aff8bb380d7ca24240596646d
sriniakhil13 <sriniakhil13@gmail.com>
2016-11-18 21:27:45 +0530

    popup.js: Change '==' to '==='
    remove '==' for '==='
    
    Fixes #31

--- popup.js
+++ popup.js
@@ -4,15 +4,15 @@ function register(){
 	
 	var query = document.getElementById("query").value;
 
-	if(document.getElementById("music").checked == true)
+	if(document.getElementById("music").checked === true)
 		set1='mp3|wav|m4a|ogg|wma|flac';
 	else
 		set1='';
-	if(document.getElementById("video").checked == true)
+	if(document.getElementById("video").checked === true)
 		var set2='|mkv|mp4|avi|webm|flv|mov|mpg|mpeg';
 	else
 		set2='';
-   	if(document.getElementById("books").checked == true)
+   	if(document.getElementById("books").checked === true)
 		var set3='|epub|pdf';
 	else
 		set3='';
