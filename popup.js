006124ee1df9922533ef72d01fbd2fd0162ce3e9
the-courier <vinay.kudari30@gmail.com>
2016-11-18 15:08:49 +0530

    Added colors to Fearch

--- popup.js
+++ popup.js
@@ -9,15 +9,19 @@ function register(){
 	else
 		set1='';
 	if(document.getElementById("video").checked == true)
-		var set2='|mkv|mp4|avi';
+		var set2='|mkv|mp4|avi|mpeg|mov|3gp';
 	else
 		set2='';
    	if(document.getElementById("books").checked == true)
-		var set3='|epub|pdf';
+		var set3='|epub|pdf|doc';
 	else
 		set3='';
+	if(document.getElementById("software").checked == true)
+		var set4='|exe|dmg|apk|ipa|msi';
+	else
+		set4='';
 
-	window.open("http://www.google.com/webhp?#q="+query+" -inurl:(htm|html|php|pls|txt) intitle:index.of \"last modified\" ("+set1+set2+set3+")&btnI=I");
+	window.open("http://www.google.com/webhp?#q="+query+" -inurl:(htm|html|php|pls|txt) intitle:index.of \"last modified\" ("+set1+set2+set3+set4+")&btnI=I");
 }
 
 document.addEventListener('DOMContentLoaded', function () {document.querySelector('button').addEventListener('click', register); });
