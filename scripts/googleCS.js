8f8f74149f45ee27a372b945f8eb5ecf6ba7641d
djmgit <djmdeveloper060796@gmail.com>
2017-10-21 14:42:38 +0530

    popup.html, popup.css, popup.js, googleFearch.js, googleCS.js: reorganised files into folders.
    Reorganised the different files into folders. Created a separate
    folder to hold the files containing the popup code.
    
    Fixes #29

--- scripts/googleCS.js
+++ scripts/googleCS.js
@@ -1,5 +1,5 @@
 var s = document.createElement("script");
-s.src = chrome.extension.getURL("googleFearch.js");
+s.src = chrome.extension.getURL("scripts/googleFearch.js");
 (document.head || document.documentElement).appendChild(s);
 
 // After loading and execution, the node disappears so no one can trace code.
