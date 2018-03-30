282746a17f13a0728d0e55d6136a67852a1a41ae
ZeroDot1 <zerodot1@bk.ru>
2018-03-30 20:21:42 +0200

    Beautify code

--- scripts/googleCS.js
+++ scripts/googleCS.js
@@ -3,6 +3,6 @@ s.src = chrome.extension.getURL("scripts/googleFearch.js");
 (document.head || document.documentElement).appendChild(s);
 
 // After loading and execution, the node disappears so no one can trace code.
-s.onload = function () {
-    s.parentNode.removeChild(s);
+s.onload = function() {
+  s.parentNode.removeChild(s);
 };
